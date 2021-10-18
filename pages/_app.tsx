91e02158a22a165ba3db907e9aded2a320968050
JSKeum <jskeum2512@gmail.com>
2021-10-18 20:50:12 +0900

    add font with global css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,22 @@
-import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { QueryClient, QueryClientProvider } from "react-query"
+import { css, Global } from "@emotion/react"
+import { appleSDGNeo } from "styles/fonts"
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient()
   return (
     <QueryClientProvider client={queryClient}>
+      <Global
+        styles={css`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            ${appleSDGNeo}
+          }
+        `}
+      />
       <Component {...pageProps} />
     </QueryClientProvider>
   )
