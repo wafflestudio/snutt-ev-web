b69d99e9c460ba6e622aece9fa85ca37e48627f6
Michael Seo <michael.seo@alphanonce.com>
2021-10-29 10:23:51 +0900

    merge with main

---
+++ lib/components/Text/index.ts
@@ -0,0 +1,14 @@
+import { Title01, Title02 } from "./Title"
+import { Subheading01, Subheading02 } from "./Subheading"
+import { Tag } from "./Tag"
+import { Detail, DetailHighlight } from "./Detail"
+
+export {
+  Title01,
+  Title02,
+  Subheading01,
+  Subheading02,
+  Tag,
+  Detail,
+  DetailHighlight,
+}
---
+++ lib/styles/fonts.ts
@@ -0,0 +1,15 @@
+import { css } from "@emotion/react"
+
+export const appleSDGNeo = css`
+  @font-face {
+    font-family: "AppleSDGothicNeo";
+    src: url("/fonts/AppleSDGothicNeoM.ttf");
+    font-weight: normal;
+  }
+
+  @font-face {
+    font-family: "AppleSDGothicNeo";
+    src: url("/fonts/AppleSDGothicNeoB.ttf");
+    font-weight: bold;
+  }
+`
