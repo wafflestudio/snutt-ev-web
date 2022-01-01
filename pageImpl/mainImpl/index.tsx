c61996f20237db647de23a2d399ddd9e3cd4a022
mike0295 <m.seo1999@gmail.com>
2022-01-01 17:47:16 +0900

    add main view category picker

--- pageImpl/mainImpl/index.tsx
+++ pageImpl/mainImpl/index.tsx
@@ -4,10 +4,36 @@ import {
   useMainRecentContainer,
   useMainReviewContainer,
 } from "./__containers__"
-import { Subheading02 } from "@lib/components/Text"
+import { Detail, Subheading02, Title02, Tag } from "@lib/components/Text"
 import { RecentCarousel } from "./__components__/RecentCarousel"
+import { useState } from "react"
+import { ToggleButton, ToggleButtonGroup } from "@mui/material/"
+
+enum LectureCategory {
+  RECOMMEND,
+  FAMOUS,
+  HONEY,
+  BITTERSWEET,
+}
+
+var lectureCategoryText = {
+  [LectureCategory.RECOMMEND]: "학우들의 추천 강의",
+  [LectureCategory.FAMOUS]: "졸업하기 전에 꼭 한번 들어볼만한 강의",
+  [LectureCategory.HONEY]: "수업 부담이 크지 않고, 성적도 잘 주는 강의",
+  [LectureCategory.BITTERSWEET]: "공과 시간을 들인 만큼 거두는 것이 많은 강의",
+}
 
 export const MainImpl = () => {
+  const [lectureCategory, setLectureCategory] = useState(
+    LectureCategory.RECOMMEND,
+  )
+
+  const onClickCategory = (e, category: LectureCategory) => {
+    if (category !== null) {
+      setLectureCategory(category)
+    }
+  }
+
   const { curationData } = useMainReviewContainer()
   const { recentLectureData } = useMainRecentContainer()
 
@@ -18,6 +44,23 @@ export const MainImpl = () => {
       ) : (
         <Subheading02>데이터 로딩 OR 에러</Subheading02>
       )}
+
+      <CategoryPicker>
+        <Title02 style={{ marginBottom: 10 }}>교양 강의평 둘러보기</Title02>
+        <StyledToggleButtonGroup
+          value={lectureCategory}
+          exclusive
+          onChange={onClickCategory}
+        >
+          <ToggleButton value={LectureCategory.RECOMMEND}>추천</ToggleButton>
+          <ToggleButton value={LectureCategory.FAMOUS}>명강</ToggleButton>
+          <ToggleButton value={LectureCategory.HONEY}>꿀강</ToggleButton>
+          <ToggleButton value={LectureCategory.BITTERSWEET}>
+            고진감래
+          </ToggleButton>
+        </StyledToggleButtonGroup>
+        <CategoryDetail>{lectureCategoryText[lectureCategory]}</CategoryDetail>
+      </CategoryPicker>
 
       {curationData ? (
         curationData.map((it) => <ReviewCard review={it} key={it.id} />)
@@ -30,12 +73,36 @@ export const MainImpl = () => {
 
 const Wrapper = styled.div``
 
-const SearchResultList = styled.div`
-  padding-left: 20px;
-  padding-right: 20px;
+const CategoryPicker = styled.div`
+  padding: 20px 20px 0 20px;
 `
 
-const SearchNoResult = styled.div`
-  font-size: 16px;
-  color: #777777;
+const CategoryDetail = styled(Detail)`
+  margin-top: 10px;
+  color: #b3b3b3;
 `
+const StyledToggleButtonGroup = styled(ToggleButtonGroup)(() => ({
+  "& .MuiToggleButtonGroup-grouped": {
+    marginRight: 10,
+    border: 0,
+    "&.Mui-disabled": {
+      border: "1px solid #777777",
+      height: 30,
+    },
+    "&.Mui-selected": {
+      border: "1px solid #777777",
+      backgroundColor: "#777777",
+      color: "#FFFFFF",
+    },
+    "&:not(:first-of-type)": {
+      border: "1px solid #777777",
+      borderRadius: 15,
+      height: 30,
+    },
+    "&:first-of-type": {
+      border: "1px solid #777777",
+      borderRadius: 15,
+      height: 30,
+    },
+  },
+}))
