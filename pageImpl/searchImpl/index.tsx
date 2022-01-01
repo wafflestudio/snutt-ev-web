8fde2ec29baf00ef03f10eef1171d9ab8f17e4ab
Jeongrok (Michael) Seo <m.seo1999@gmail.com>
2022-01-01 18:49:25 +0900

    [SNUTT-232] 메인뷰 교양 강의평 카테고리 선택  (#11)
    * Add main page recent lecture carousel
    
    * Change normal font from M -> L
    
    * add icons
    
    * complete carousel card content layout
    
    * change lexture card to flex
    
    * add main view category picker
    
    * reformat searchImpl code, update yarn files
    
    * edit var -> const
    
    * refactor css
    
    * add buttongroup error handle
    
    Co-authored-by: Michael Seo <michael.seo@alphanonce.com>

--- pageImpl/searchImpl/index.tsx
+++ pageImpl/searchImpl/index.tsx
@@ -4,32 +4,30 @@ import { useSearchContainer } from "./__containers__/search"
 import { SearchResultItem } from "./__components__/SearchResultItem"
 
 export const SearchImpl = () => {
-    const { data } = useSearchContainer()
+  const { data } = useSearchContainer()
 
-    return (
-        <Wrapper>
-            <Searchbar />
-            <SearchResultList>
-                {
-                    data ? (
-                        data.map((it) => <SearchResultItem content={it} key={it.id}/>)
-                    ) : (
-                        <SearchNoResult>강의명, 교수명으로 검색하세요</SearchNoResult>
-                    )
-                }
-            </SearchResultList>
-        </Wrapper>
-    )
+  return (
+    <Wrapper>
+      <Searchbar />
+      <SearchResultList>
+        {data ? (
+          data.map((it) => <SearchResultItem content={it} key={it.id} />)
+        ) : (
+          <SearchNoResult>강의명, 교수명으로 검색하세요</SearchNoResult>
+        )}
+      </SearchResultList>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.div``
 
 const SearchResultList = styled.div`
-    padding-left: 20px;
-    padding-right: 20px;
+  padding-left: 20px;
+  padding-right: 20px;
 `
 
 const SearchNoResult = styled.div`
-    font-size: 16px;
-    color: #777777;
+  font-size: 16px;
+  color: #777777;
 `
