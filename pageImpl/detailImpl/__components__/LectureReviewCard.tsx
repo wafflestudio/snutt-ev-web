5b3ddd7405141de13877ff9baf6b55876c9b883e
Jeongrok (Michael) Seo <m.seo1999@gmail.com>
2022-01-08 17:53:10 +0900

    [SNUTT-265] 강의평 디테일뷰 구현 (#15)
    * add Appbar component
    
    * add Appbar to main view
    
    * implement star rating for main view
    
    * minor rating logic change
    
    * implement sticky app bar in main
    
    * apply appbar to search view
    
    * implement review detail
    
    * add expanding detail card

---
+++ pageImpl/detailImpl/__components__/LectureReviewCard.tsx
@@ -0,0 +1,86 @@
+import styled from "@emotion/styled"
+import { Detail, Subheading01 } from "@lib/components/Text"
+import { COLORS } from "@lib/styles/colors"
+import { ReviewDetailDTO } from "@lib/dto/review"
+import { Rating } from "@lib/components/Rating"
+import ShowMoreText from "react-show-more-text"
+import { useState } from "react"
+
+interface Props {
+  review: ReviewDetailDTO
+}
+
+export const LectureReviewCard = ({ review }: Props) => {
+  const [expanded, setExpanded] = useState(false)
+  const truncBy = 120
+
+  const text =
+    review.contents.length > truncBy && expanded
+      ? review.contents.slice(0, truncBy) + "..."
+      : review.contents
+
+  return (
+    <Wrapper>
+      <Contents>
+        <Header>
+          <Rating rating={review.point} />
+          <SideInfo>
+            <Semester>{review.semester} 수강</Semester>
+          </SideInfo>
+        </Header>
+        <Review>
+          <Detail>
+            {text}
+            {review.contents.length > truncBy && (
+              <MoreLessButton onClick={() => setExpanded((status) => !status)}>
+                {" "}
+                {expanded ? "더보기" : "접기"}
+              </MoreLessButton>
+            )}
+          </Detail>
+        </Review>
+      </Contents>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 20px 0 5px 0;
+  box-sizing: border-box;
+`
+
+const Contents = styled.div`
+  border-bottom: 1px solid ${COLORS.gray};
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 15px;
+`
+
+const Header = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 12px;
+`
+
+const Semester = styled(Detail)`
+  color: ${COLORS.darkGray};
+  text-align: right;
+`
+
+const SideInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Review = styled.div`
+  display: inline-block;
+  width: 100%;
+`
+
+const MoreLessButton = styled.a`
+  color: rgb(179, 179, 179);
+`
