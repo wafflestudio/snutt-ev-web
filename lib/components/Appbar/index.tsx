5314174d72b0daab995d8b473bd8983239d84971
mike0295 <m.seo1999@gmail.com>
2022-01-16 16:13:29 +0900

    rebasing...

--- lib/components/Appbar/index.tsx
+++ lib/components/Appbar/index.tsx
@@ -1,5 +1,4 @@
 import styled from "@emotion/styled"
-<<<<<<< HEAD
 import { ReactNode } from "react"
 
 interface Props {
@@ -12,28 +11,6 @@ export const AppBar = ({ LeftImage, children }: Props) => {
     <Wrapper>
       <AppBarLeft>
         <LeftImage />
-=======
-import Image from "next/image"
-import { ReactNode } from "react"
-
-interface Props {
-  leftImageSrc: string
-  leftImageOnClick: () => void
-  children: ReactNode
-}
-
-export const AppBar = ({ leftImageSrc, leftImageOnClick, children }: Props) => {
-  return (
-    <Wrapper>
-      <AppBarLeft>
-        <LeftImage
-          src={leftImageSrc}
-          alt={"logo"}
-          height={30}
-          width={30}
-          onClick={leftImageOnClick}
-        />
->>>>>>> 6cfe3b3 (add Appbar component)
       </AppBarLeft>
       {children}
     </Wrapper>
@@ -47,16 +24,12 @@ const Wrapper = styled.div`
   order: 1;
   height: 45px;
 
-<<<<<<< HEAD
   position: sticky;
   top: 0;
 
   background-color: white;
   border-bottom: solid 1px rgba(179, 179, 179, 0.3);
   z-index: 99;
-=======
-  border-bottom: solid 1px rgba(151, 151, 151, 0.5);
->>>>>>> 6cfe3b3 (add Appbar component)
 `
 
 const AppBarLeft = styled.div`
@@ -65,8 +38,3 @@ const AppBarLeft = styled.div`
   align-items: center;
   margin-left: 13px;
 `
-<<<<<<< HEAD
-=======
-
-const LeftImage = styled(Image)``
->>>>>>> 6cfe3b3 (add Appbar component)
