22e5b76f7cfe30fd4be7de2d8e6974cbb8feb50d
mike0295 <m.seo1999@gmail.com>
2022-01-22 18:42:24 +0900

    Revert "Revert "Revert "temporary 404 (#29)"""
    This reverts commit 1d9cbcbbfe99bda1a6588061c137d7ff60b7728f.

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,8 +1,6 @@
 import React from "react"
 import { MainImpl } from "@pageImpl/mainImpl"
-import Custom404 from "pages/404"
 
 export default function MainView() {
-  // return <MainImpl />
-  return <Custom404 />
+  return <MainImpl />
 }
