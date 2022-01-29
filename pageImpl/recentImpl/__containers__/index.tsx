dfb4a870ac39a9a9aa67b3bdecdc67bbbfffb82d
Jeongrok (Michael) Seo <m.seo1999@gmail.com>
2022-01-30 00:29:53 +0900

    feat: latest lecture implement (#52)

--- pageImpl/recentImpl/__containers__/index.tsx
+++ pageImpl/recentImpl/__containers__/index.tsx
@@ -1,8 +1,8 @@
-import { fetchRecentLectures } from "@lib/api/apis"
+import { fetchLatestLectures } from "@lib/api/apis"
 import { useQuery } from "react-query"
 
 export function useRecentLectureContainer() {
-  const querySearch = useQuery("recent/", fetchRecentLectures)
+  const querySearch = useQuery("recent/", fetchLatestLectures)
 
   const { data, error } = querySearch
 
