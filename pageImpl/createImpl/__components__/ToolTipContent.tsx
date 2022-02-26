aa6e951be38a3e9812eaf6196ad42d21bde9f6d6
Jinsup Keum <54926767+JSKeum@users.noreply.github.com>
2022-02-26 16:06:53 +0900

    Tooltip 수정 [SNUTT-327] (#73)
    * add tooltip
    
    * fix tooltip content with triangle
    
    * fix RatingTooltip to catch when click outside button
    
    * fix z-index for tooltip to cover inner contents
    
    * fix create polygon position
    
    * fix build error wit typescript
    
    * fix ref var name
    
    Co-authored-by: mike0295 <m.seo1999@gmail.com>

--- pageImpl/createImpl/__components__/ToolTipContent.tsx
+++ pageImpl/createImpl/__components__/ToolTipContent.tsx
@@ -1,9 +1,9 @@
 import { Title01, Title02 } from "@lib/components/Text"
-import { Fragment } from "react"
+import styled from "@emotion/styled"
 
 export const TootTipContent = () => {
   return (
-    <Fragment>
+    <Wrapper>
       <Title01>성적만족도</Title01>
       <Title02 style={{ marginBottom: 10 }}>
         이번 강의 성적에 만족하셨나요?
@@ -18,6 +18,41 @@ export const TootTipContent = () => {
       </Title02>
       <Title01>얻어가는 것</Title01>
       <Title02>강의를 통해 배움을 얻으셨나요?</Title02>
-    </Fragment>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  & {
+    position: absolute;
+    background: #f2f2f2;
+    border: 1px solid #c4c4c4;
+    padding: 14px;
+    width: 248px;
+    right: 0;
+    top: 48px;
+    border-radius: 6px;
+  }
+
+  &:before {
+    top: -15px;
+    right: 3px;
+    content: "";
+    border-right: 11px solid transparent;
+    border-left: 11px solid transparent;
+    border-bottom: 24px solid #f2f2f2;
+    position: absolute;
+    z-index: 10;
+  }
+
+  &:after {
+    top: -16px;
+    right: 1px;
+    content: "";
+    border-right: 13px solid transparent;
+    border-left: 13px solid transparent;
+    border-bottom: 26px solid #c4c4c4;
+    position: absolute;
+    z-index: -10;
+  }
+`
