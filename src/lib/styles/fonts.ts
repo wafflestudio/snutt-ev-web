ce39f9ea10a86d84a70678f502533dd3e040b642
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2022-09-15 13:10:14 +0900

    :zap: font public/static/ 으로 옮기기 (#128)

--- src/lib/styles/fonts.ts
+++ src/lib/styles/fonts.ts
@@ -3,13 +3,13 @@ import { css } from "@emotion/react"
 export const appleSDGNeo = css`
   @font-face {
     font-family: "AppleSDGothicNeo";
-    src: url("/fonts/AppleSDGothicNeoL.ttf");
+    src: url("/static/fonts/AppleSDGothicNeoL.ttf");
     font-weight: normal;
   }
 
   @font-face {
     font-family: "AppleSDGothicNeo";
-    src: url("/fonts/AppleSDGothicNeoB.ttf");
+    src: url("/static/fonts/AppleSDGothicNeoB.ttf");
     font-weight: bold;
   }
 `
