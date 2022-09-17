8f825884f4bf72550ce802462ffb87cd6fa25d5a
woohm402 <woohm404@gmail.com>
2022-09-17 16:29:51 +0900

    msw 환경 integration & main 페이지에 msw 적용

--- src/mocks/handlers/core/index.ts
+++ src/mocks/handlers/core/index.ts
@@ -1,1 +1,7 @@
-export const coreHandlers = [];
+import { rest } from 'msw';
+
+export const coreHandlers = [
+  rest.get('*/v1/user/email/verification', (req, res, ctx) => {
+    return res(ctx.json({ is_email_verified: true }));
+  }),
+];
