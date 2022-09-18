44db4d3186d75b533fcfe85108bf38a4f5bf40f2
woohm402 <woohm404@gmail.com>
2022-09-19 07:46:42 +0900

    SelectedTagList 컴포넌트 코드 분리

---
+++ src/pageImpl/searchImpl/__components__/SelectedTagList/TagItem.tsx
@@ -0,0 +1,49 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+import SvgExitWhite from '@/lib/components/Icons/SvgExitWhite';
+import { TagWithColor } from '@/lib/dto/core/tag';
+
+interface Props {
+  tag: TagWithColor;
+  onClick: () => void;
+}
+
+export const TagItem = ({ tag, onClick }: Props) => {
+  return (
+    <TagItemBox onClick={onClick} $color={tag.color}>
+      <TagText>{tag.name}</TagText>
+      <DeleteIcon width={15} height={15} />
+    </TagItemBox>
+  );
+};
+
+const TagText = styled.div`
+  font-family: AppleSDGothicNeo;
+  font-size: 14px;
+  white-space: nowrap;
+  font-weight: 400;
+  margin-right: 5px;
+  color: white;
+`;
+
+const TagItemBox = styled.div<{ $color: string | null }>`
+  border-radius: 999px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 20px;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 6px;
+  padding-bottom: 6px;
+  margin-right: 10px;
+
+  ${({ $color }) =>
+    $color &&
+    css`
+      background-color: ${$color};
+    `}
+`;
+
+const DeleteIcon = styled(SvgExitWhite)``;
---
+++ src/pageImpl/searchImpl/__components__/SelectedTagList/index.tsx
@@ -0,0 +1,29 @@
+import styled from '@emotion/styled';
+
+import { TagWithColor } from '@/lib/dto/core/tag';
+
+import { TagItem } from './TagItem';
+
+interface Props {
+  selectedTags: TagWithColor[] | undefined;
+  onDeleteTag: (tag: number) => void;
+}
+
+export const ActiveTagList = ({ selectedTags = [], onDeleteTag }: Props) => {
+  if (selectedTags.length === 0) return null;
+
+  return (
+    <Wrapper>
+      {selectedTags.map((it) => (
+        <TagItem tag={it} key={it.name} onClick={() => onDeleteTag(it.id)} />
+      ))}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  overflow: scroll;
+  display: flex;
+  flex-direction: row;
+  padding: 10px;
+`;
