12e8c8bd1b4522ce1febc8bff24c022dd2a7aebb
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2022-09-20 23:45:04 +0900

    🚚 테스트코드 파일 분리 & BDD 형태의 주석 추가 (#162)

---
+++ e2e/main/header.spec.ts
@@ -0,0 +1,18 @@
+import { expect, test } from '@playwright/test';
+
+import { MainPage } from './_page';
+
+test('헤더바가 잘 보여진다', async ({ page }) => {
+  // 페이지에 접속하면
+  const main = new MainPage(page);
+  await main.goto();
+
+  // app bar 에 강의평이라는 문구가 보여져야 한다
+  const appBar = main.findByTestId('app-bar');
+  await expect(appBar).toContainText('강의평');
+
+  // 검색 버튼을 클릭하면 검색 페이지로 이동해야 한다
+  const searchIcon = main.findByTestId('main-search-icon');
+  await searchIcon.click();
+  await expect(main.getPage()).toHaveURL('/search/');
+});
---
+++ e2e/main/tag.spec.ts
@@ -0,0 +1,46 @@
+import { expect, test } from '@playwright/test';
+
+import { MainPage } from './_page';
+
+test('교양 강의평 둘러보기', async ({ page }) => {
+  // 페이지에 접속하면
+  const main = new MainPage(page);
+  await main.goto();
+
+  // 태그 피커가 보여야 한다
+  const container = main.findByTestId('main-category-picker');
+  await expect(container).toHaveCount(1);
+
+  // 토글 기능이 정상 작동해야 한다
+  const toggleChip = main.findByTestId('main-category-toggle-chip');
+  const recentChip = toggleChip.locator('text=최신');
+  const recommendChip = toggleChip.locator('text=추천');
+  const detailText = main.findByTestId('main-category-detail');
+  await expectChipStatus('recent');
+  await recommendChip.click();
+  await expectChipStatus('recommend');
+  await recentChip.click();
+  await expectChipStatus('recent');
+
+  // helpers
+  async function expectChipStatus(status: 'recent' | 'recommend') {
+    return Promise.all([
+      expect(recentChip).toHaveAttribute(
+        'aria-selected',
+        `${status === 'recent'}`,
+      ),
+      expect(recommendChip).toHaveAttribute(
+        'aria-selected',
+        `${status === 'recommend'}`,
+      ),
+      expect(detailText).toHaveText(
+        { recent: '최근 등록된 강의평', recommend: '학우들의 추천 강의' }[
+          status
+        ],
+      ),
+      expect(main.getPage()).toHaveURL(
+        `/main/?tag=${{ recent: 1, recommend: 2 }[status]}`,
+      ),
+    ]);
+  }
+});
