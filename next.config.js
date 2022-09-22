37ce1386928a1d2e5af06a76f894dc36e28e6856
woohm402 <woohm404@gmail.com>
2022-09-23 01:02:39 +0900

    :sparkles: apply ssr

--- next.config.js
+++ next.config.js
@@ -9,6 +9,4 @@ module.exports = {
     return config;
   },
   reactStrictMode: true,
-  // SSG 배포를 하는 동안에는 Cloudfront 이슈로 trailingSlash: true 로 설정되어 있어야 함
-  trailingSlash: true,
 };
--- src/lib/api/apis.ts
+++ src/lib/api/apis.ts
@@ -1,3 +1,5 @@
+import { GetServerSidePropsContext } from 'next';
+
 import { DeleteEvaluationResult } from '@/lib/dto/deleteEvaluation';
 import { GetEmailVerificationResult } from '@/lib/dto/getEmailVerification';
 import {
@@ -32,127 +34,192 @@ import {
 } from '@/lib/dto/postReportEvaluation';
 
 import { GetTagInfosProcessedResult } from '../dto/getTagInfos';
-import { coreClient, evClient } from './request';
+import { coreClient, evClient, getServerSideHeaders } from './request';
+
+type Context = { context?: GetServerSidePropsContext };
+type Args<P = undefined, Q = undefined, B = undefined> = Context &
+  (P extends undefined ? unknown : { params: P }) &
+  (Q extends undefined ? unknown : { query: Q }) &
+  (B extends undefined ? unknown : { body: B });
 
 // 지난 학기 들은 강의 목록 불러오는 api
-export async function fetchLatestLectures() {
+export async function fetchLatestLectures(args: Args = {}) {
   const endpoint = `v1/users/me/lectures/latest`;
-  const response = await evClient.get<GetLatestLecturesResult>(endpoint);
+  const headers = getServerSideHeaders(args.context);
+
+  const response = await evClient.get<GetLatestLecturesResult>(endpoint, {
+    headers,
+  });
   return response.data;
 }
 
 // 검색 시 필요한 태그 목록 불러오는 api
-export async function fetchTagInfos() {
+export async function fetchTagInfos(args: Args = {}) {
   const endpoint = `/v1/tags/search`;
-  const response = await evClient.get<GetTagInfosProcessedResult>(endpoint);
+  const headers = getServerSideHeaders(args.context);
+
+  const response = await evClient.get<GetTagInfosProcessedResult>(endpoint, {
+    headers,
+  });
   return response.data;
 }
 
 // 강의평 생성 시 해당 강의 정보 및 해당 강의의 과거 개설 학기들 불러오는 api
-export async function fetchSemesterLectures(id: number) {
-  const endpoint = `/v1/lectures/${id}/semester-lectures`;
-  const response = await evClient.get<GetSemesterLecturesResult>(endpoint);
+export async function fetchSemesterLectures(args: Args<{ id: number }>) {
+  const endpoint = `/v1/lectures/${args.params.id}/semester-lectures`;
+  const headers = getServerSideHeaders(args.context);
+
+  const response = await evClient.get<GetSemesterLecturesResult>(endpoint, {
+    headers,
+  });
   return response.data;
 }
 
 // 강의평 생성 api
 export async function postLectureEvaluation(
-  id: number,
-  body: PostEvaluationQuery,
+  args: Args<{ id: number }, undefined, PostEvaluationQuery>,
 ) {
-  const endpoint = `/v1/semester-lectures/${id}/evaluations`;
-  const response = await evClient.post<PostEvaluationResult>(endpoint, body);
+  const endpoint = `/v1/semester-lectures/${args.params.id}/evaluations`;
+  const headers = getServerSideHeaders(args.context);
+
+  const response = await evClient.post<PostEvaluationResult>(
+    endpoint,
+    args.body,
+    { headers },
+  );
   return response.data;
 }
 
 // 강좌의 강의평 목록 api
 export async function fetchLectureEvaluations(
-  id: number,
-  params: GetEvaluationsQuery,
+  args: Args<{ id: number }, GetEvaluationsQuery>,
 ) {
-  const endpoint = `v1/lectures/${id}/evaluations`;
+  const endpoint = `v1/lectures/${args.params.id}/evaluations`;
+  const headers = getServerSideHeaders(args.context);
+
   const response = await evClient.get<GetEvaluationsResult>(endpoint, {
-    params,
+    params: args.query,
+    headers,
   });
   return response.data;
 }
 
-export async function fetchMyLectureEvaluations(id: number) {
-  const endpoint = `/v1/lectures/${id}/evaluations/users/me`;
-  const response = await evClient.get<GetMyEvaluationsResult>(endpoint);
+export async function fetchMyLectureEvaluations(args: Args<{ id: number }>) {
+  const endpoint = `/v1/lectures/${args.params.id}/evaluations/users/me`;
+  const headers = getServerSideHeaders(args.context);
+
+  const response = await evClient.get<GetMyEvaluationsResult>(endpoint, {
+    headers,
+  });
   return response.data;
 }
 
-export async function fetchEvaluationSummary(id: number) {
-  const endpoint = `/v1/lectures/${id}/evaluation-summary`;
-  const response = await evClient.get<GetEvaluationSummaryResponse>(endpoint);
+export async function fetchEvaluationSummary(args: Args<{ id: number }>) {
+  const endpoint = `/v1/lectures/${args.params.id}/evaluation-summary`;
+  const headers = getServerSideHeaders(args.context);
+
+  const response = await evClient.get<GetEvaluationSummaryResponse>(endpoint, {
+    headers,
+  });
   return response.data;
 }
 
-export async function getLectures(query: GetLecturesQuery) {
+export async function getLectures(args: Args<undefined, GetLecturesQuery>) {
   const endpoint = `/v1/lectures`;
-  const params = { ...query, tags: query.tags.join(',') };
-  const response = await evClient.get<GetLecturesResult>(endpoint, { params });
+  const headers = getServerSideHeaders(args.context);
+
+  const params = { ...args.query, tags: args.query.tags.join(',') };
+
+  const response = await evClient.get<GetLecturesResult>(endpoint, {
+    params,
+    headers,
+  });
   return response.data;
 }
 
-export async function getMainTagInfos() {
+export async function getMainTagInfos(args: Args = {}) {
   const endpoint = `/v1/tags/main`;
-  const response = await evClient.get<GetMainTagInfosResult>(endpoint);
+  const headers = getServerSideHeaders(args.context);
+
+  const response = await evClient.get<GetMainTagInfosResult>(endpoint, {
+    headers,
+  });
   return response.data;
 }
 
 export async function getMainTagEvaluations(
-  id: number,
-  params: GetMainTagEvalutionsQuery,
+  args: Args<{ id: number }, GetMainTagEvalutionsQuery>,
 ) {
-  const endpoint = `/v1/tags/main/${id}/evaluations`;
+  const endpoint = `/v1/tags/main/${args.params.id}/evaluations`;
+  const headers = getServerSideHeaders(args.context);
+
   const response = await evClient.get<GetMainTagEvaluationsResult>(endpoint, {
-    params,
+    params: args.query,
+    headers,
   });
   return response.data;
 }
 
-export async function deleteEvaluation(id: number) {
-  const endpoint = `/v1/evaluations/${id}`;
-  const response = await evClient.delete<DeleteEvaluationResult>(endpoint);
+export async function deleteEvaluation(args: Args<{ id: number }>) {
+  const endpoint = `/v1/evaluations/${args.params.id}`;
+  const headers = getServerSideHeaders(args.context);
+
+  const response = await evClient.delete<DeleteEvaluationResult>(endpoint, {
+    headers,
+  });
   return response.data;
 }
 
 export async function postReportEvaluation(
-  id: number,
-  body: PostReportEvaluationParams,
+  args: Args<{ id: number }, undefined, PostReportEvaluationParams>,
 ) {
-  const endpoint = `/v1/evaluations/${id}/report`;
+  const endpoint = `/v1/evaluations/${args.params.id}/report`;
+  const headers = getServerSideHeaders(args.context);
+
   const response = await evClient.post<PostReportEvaluationResult>(
     endpoint,
-    body,
+    args.body,
+    { headers },
   );
   return response.data;
 }
 
-export async function getEmailVerification(): Promise<GetEmailVerificationResult> {
+export async function getEmailVerification(
+  args: Args,
+): Promise<GetEmailVerificationResult> {
   const endpoint = `/v1/user/email/verification`;
-  const response = await coreClient.get<GetEmailVerificationResult>(endpoint);
+  const headers = getServerSideHeaders(args.context);
+
+  const response = await coreClient.get<GetEmailVerificationResult>(endpoint, {
+    headers,
+  });
   return response.data;
 }
 
-export async function postEmailVerification(body: PostEmailVerificationParams) {
+export async function postEmailVerification(
+  args: Args<undefined, undefined, PostEmailVerificationParams>,
+) {
   const endpoint = `/v1/user/email/verification`;
+  const headers = getServerSideHeaders(args.context);
+
   const response = await coreClient.post<PostEmailVerificationResult>(
     endpoint,
-    body,
+    args.body,
+    { headers },
   );
   return response.data;
 }
 
 export async function postEmailVerificationCode(
-  body: PostEmailVerificationCodeParams,
+  args: Args<undefined, undefined, PostEmailVerificationCodeParams>,
 ) {
   const endpoint = `/v1/user/email/verification/code`;
+  const headers = getServerSideHeaders(args.context);
+
   const response = await coreClient.post<PostEmailVerificationCodeResult>(
     endpoint,
-    body,
+    args.body,
+    { headers },
   );
   return response.data;
 }
