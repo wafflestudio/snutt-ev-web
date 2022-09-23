6d2a8c6e49dad481aa897f02d5ba91169113a98d
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2022-09-24 00:26:11 +0900

    :zap: 디폴트로 시스템 폰트 이용 (#178)

--- src/lib/styles/fonts.ts
+++ src/lib/styles/fonts.ts
@@ -5,11 +5,13 @@ export const appleSDGNeo = css`
     font-family: 'AppleSDGothicNeo';
     src: url('/static/fonts/AppleSDGothicNeoL.ttf');
     font-weight: normal;
+    font-display: swap;
   }
 
   @font-face {
     font-family: 'AppleSDGothicNeo';
     src: url('/static/fonts/AppleSDGothicNeoB.ttf');
     font-weight: bold;
+    font-display: swap;
   }
 `;
