983b5a252cbacfef046b32a973429b16d16ab056
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2022-09-25 23:22:16 +0900

    🤡 core client 모킹 구현 (#203)

--- src/mocks/handlers/core/index.ts
+++ src/mocks/handlers/core/index.ts
@@ -1,7 +1,24 @@
 import { rest } from 'msw';
 
 export const coreHandlers = [
-  rest.get('*/v1/user/email/verification', (req, res, ctx) => {
-    return res(ctx.json({ is_email_verified: true }));
+  rest.get<never, never, { is_email_verified: boolean }>('*/v1/user/email/verification', (req, res, ctx) => {
+    const { TEST_USER_EMAIL_VERIFIED = 'true' } = req.cookies;
+
+    return res(ctx.json({ is_email_verified: TEST_USER_EMAIL_VERIFIED === 'true' }));
   }),
+
+  rest.post<{ email: string }, never, { message: 'ok' }>('*/v1/user/email/verification', (req, res, ctx) => {
+    if (!req.body.email.includes('@snu.ac.kr')) return res(ctx.status(400));
+
+    return res(ctx.json({ message: 'ok' }));
+  }),
+
+  rest.post<{ code: number }, never, { is_email_verified: boolean }>(
+    '*/v1/user/email/verification/code',
+    (req, res, ctx) => {
+      // TODO: 고민 필요, 실패 케이스가 너무 많은데, 그걸 다 테스트할 건지? 테스트할 거라면, cookie 값은 TEST_USER_EMAIL_VERIFY_RESULT 로 하고 enum 으로 가야 할듯
+
+      return res(ctx.json({ is_email_verified: true }));
+    },
+  ),
 ];
