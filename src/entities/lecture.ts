a7b6b28a343ea20eb9943ac296c7cbf9142af07f
woohm402 <woohm404@gmail.com>
2022-09-26 09:08:09 +0900

    semester entity 생성

---
+++ src/entities/lecture.ts
@@ -0,0 +1,1 @@
+export type Semester = 1 | 2 | 3 | 4;
---
+++ src/usecases/lecture/semesterToString/index.ts
@@ -0,0 +1,16 @@
+import { Semester } from '@/entities/lecture';
+
+export const semesterToString = (semester: Semester) => {
+  switch (semester) {
+    case 1:
+      return '1';
+    case 2:
+      return '여름';
+    case 3:
+      return '2';
+    case 4:
+      return '겨울';
+    default:
+      return '';
+  }
+};
