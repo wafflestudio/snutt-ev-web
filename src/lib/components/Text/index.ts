e03ec5533e343a3494be678efb0794577d63885f
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2022-09-30 23:16:13 +0900

    ⚰️ 안 쓰는 코드 제거 (#215)

--- src/lib/components/Text/index.ts
+++ src/lib/components/Text/index.ts
@@ -1,6 +1,3 @@
-import { Detail, DetailHighlight } from './Detail';
-import { Subheading01, Subheading02 } from './Subheading';
-import { Tag } from './Tag';
-import { Title01, Title02 } from './Title';
-
-export { Title01, Title02, Subheading01, Subheading02, Tag, Detail, DetailHighlight };
+export * from './Detail';
+export * from './Subheading';
+export * from './Title';
