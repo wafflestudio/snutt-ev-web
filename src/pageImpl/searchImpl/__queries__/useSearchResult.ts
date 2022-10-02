336d64d39065a27bbb560520f6aaecf48f27dc2b
woohm402 <woohm404@gmail.com>
2022-10-03 01:02:16 +0900

    검색 페이지 query-container 정리

---
+++ src/pageImpl/searchImpl/__queries__/useSearchResult.ts
@@ -0,0 +1,11 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+import { getLectures } from '@/lib/apis/ev';
+
+export const useSearchResult = (selectedTagIDs: number[], textQuery?: string) => {
+  return useInfiniteQuery(
+    ['lectureSearch', textQuery, selectedTagIDs],
+    ({ pageParam }) => getLectures({ query: { query: textQuery, tags: selectedTagIDs, page: pageParam } }),
+    { getNextPageParam: (lastPage) => (lastPage.last ? undefined : (lastPage.page ?? 0) + 1) },
+  );
+};
