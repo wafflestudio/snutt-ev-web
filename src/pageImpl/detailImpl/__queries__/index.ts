c331c14f062b3a7018105d7e8119336553b1e034
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2022-10-03 11:37:13 +0900

    🚸 detail page full ssr 적용 (#231)

--- src/pageImpl/detailImpl/__queries__/index.ts
+++ src/pageImpl/detailImpl/__queries__/index.ts
@@ -1,5 +1,8 @@
+// queries
 export * from './useEvaluationSummary';
 export * from './useLectureMyEvaluations';
+export * from './useLectureEvaluations';
+
+// mutations
 export * from './useReportEvaluation';
 export * from './useDeleteEvaluation';
-export * from './useLectureEvaluations';
--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,3 +1,4 @@
+import { fetchEvaluationSummary, fetchLectureEvaluations, fetchMyLectureEvaluations } from '@/lib/apis/ev';
 import { withGetServerSideProps } from '@/lib/util/withGetServersideProps';
 import { DetailImpl } from '@/pageImpl/detailImpl';
 
@@ -6,7 +7,21 @@ export default function DetailView() {
 }
 
 export const getServerSideProps = withGetServerSideProps(
-  async () => {
+  async (context, { queryClient }) => {
+    const id = Number(context.query.id);
+
+    await Promise.all([
+      queryClient.prefetchQuery(['evaluationSummary', id] as const, ({ queryKey: [, id] }) =>
+        fetchEvaluationSummary({ params: { id }, context }),
+      ),
+      queryClient.prefetchQuery(['myLectureEvaluation', id] as const, async ({ queryKey: [, id] }) =>
+        fetchMyLectureEvaluations({ params: { id }, context }),
+      ),
+      queryClient.prefetchInfiniteQuery(['lectureEvaluation', id], async ({ pageParam }) =>
+        fetchLectureEvaluations({ params: { id }, query: { cursor: pageParam }, context }),
+      ),
+    ]);
+
     return { props: {} };
   },
   { emailVerification: 'verified' },
