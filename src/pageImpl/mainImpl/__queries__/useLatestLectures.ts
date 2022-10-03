b01ccc319b93fc7ecdec8d72257d7aa956f10c12
woohm402 <woohm404@gmail.com>
2022-10-03 22:33:56 +0900

    메인 페이지에서 내가 강의평을 썼으면 그건 제외하고 보여주기

--- src/pageImpl/mainImpl/__queries__/useLatestLectures.ts
+++ src/pageImpl/mainImpl/__queries__/useLatestLectures.ts
@@ -3,5 +3,7 @@ import { useQuery } from '@tanstack/react-query';
 import { fetchLatestLectures } from '@/lib/apis/ev';
 
 export function useLatestLectures() {
-  return useQuery(['latestLectures'], () => fetchLatestLectures());
+  return useQuery(['latestLectures', { filter: 'no-my-evaluation' }], () =>
+    fetchLatestLectures({ query: { filter: 'no-my-evaluations' } }),
+  );
 }
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -10,7 +10,9 @@ export const getServerSideProps = withGetServerSideProps(
   async (context, { queryClient }) => {
     await Promise.all([
       queryClient.prefetchQuery(['mainTags'], () => getMainTagInfos({ context })),
-      queryClient.prefetchQuery(['latestLectures'], () => fetchLatestLectures({ context })),
+      queryClient.prefetchQuery(['latestLectures', { filter: 'no-my-evaluation' }], () =>
+        fetchLatestLectures({ context, query: { filter: 'no-my-evaluations' } }),
+      ),
     ]);
     return { props: {} };
   },
