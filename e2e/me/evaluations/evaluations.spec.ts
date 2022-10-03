13011241d1915cc91a41e798ac6568d5f3c6967f
woohm402 <woohm404@gmail.com>
2022-10-03 22:16:48 +0900

    내가 쓴 강의평 개수 보여주기

--- e2e/me/evaluations/evaluations.spec.ts
+++ e2e/me/evaluations/evaluations.spec.ts
@@ -4,6 +4,8 @@ import { getTestCookie } from '../../utils/cookie';
 import { withCookie } from '../../utils/withCookiePage';
 import { MyEvaluationsPage } from './_page';
 
+const totalCountTestId = 'my-evaluations-total-count';
+
 test(
   '강의평 목록이 비어있을 경우',
   withCookie(
@@ -21,6 +23,10 @@ test(
       // 강의평은 안 보여야 한다
       const ev = main.findByTestId('my-evaluations-evaluation-card');
       await expect(ev).toHaveCount(0);
+
+      // 내가 작성한 강의평 개수 영역이 잘 나타나야 한다
+      const tc = main.findByTestId(totalCountTestId);
+      await expect(tc).toHaveText('(0개)');
     },
   ),
 );
@@ -44,6 +50,10 @@ test(
 
       // 첫 번째 강의평의 교수명이 잘 나타나야 한다
       await expect(firstEv).toContainText('염헌영');
+
+      // 내가 작성한 강의평 개수 영역이 잘 나타나야 한다
+      const tc = main.findByTestId(totalCountTestId);
+      await expect(tc).toHaveText('(12개)');
 
       // 첫 번째 강의평을 클릭하면 해당하는 페이지로 이동해야 한다
       await firstEv.locator('text=시스템프로그래밍').click();
