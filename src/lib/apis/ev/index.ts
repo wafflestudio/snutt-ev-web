74c5efc4c621afae81748659c1832937ba8c0010
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2022-11-26 17:19:09 +0900

    🎉 메인 페이지 prod 에서도 최근에 들은 강의 필터 파라미터 보내기 (#279)

--- src/lib/apis/ev/index.ts
+++ src/lib/apis/ev/index.ts
@@ -1,6 +1,5 @@
 import { evClient } from '@/clients/axiosEvClient';
 import { Args } from '@/lib/util/apiArgs';
-import { APP_ENV } from '@/lib/util/env';
 import { getServerSideHeaders } from '@/lib/util/getServerSideHeaders';
 
 import type {
@@ -29,10 +28,7 @@ export async function fetchLatestLectures(args: Args<undefined, { filter?: 'no-m
   const endpoint = `/v1/users/me/lectures/latest`;
   const headers = getServerSideHeaders(args.context);
 
-  const response = await evClient.get<GetLatestLecturesResult>(
-    endpoint,
-    APP_ENV === 'prod' ? { headers } : { headers, params: args.query },
-  );
+  const response = await evClient.get<GetLatestLecturesResult>(endpoint, { headers, params: args.query });
   return response.data;
 }
 
