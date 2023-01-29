0b21b6d48d3c277cbd08429c53c9106b10e82fb1
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2023-01-29 20:50:11 +0900

    🚸 강의평 카드 클릭 시 내 강의평으로 넘어가는 클릭 영역 확대 (#342)

--- src/components/organisms/CollapsableText.tsx
+++ src/components/organisms/CollapsableText.tsx
@@ -1,25 +1,32 @@
 import styled from '@emotion/styled';
-import { useState } from 'react';
+import { MouseEvent, useState } from 'react';
 
 import { Detail } from '@/components/atoms/Typography';
 
 interface Props {
   text: string;
   truncBy?: number;
+  onClickMoreLess?: (e: MouseEvent) => void;
 }
 
-export const CollapsableText = ({ text, truncBy = 120 }: Props) => {
+export const CollapsableText = ({ text, truncBy = 120, onClickMoreLess }: Props) => {
   const [expanded, setExpanded] = useState(false);
 
   const renderedText = text.length > truncBy && !expanded ? text.slice(0, truncBy) + '...' : text;
 
-  const onClickMoreLess = () => setExpanded((status) => !status);
+  const handleClickMoreLess = () => setExpanded((status) => !status);
 
   return (
     <Detail style={{ overflowWrap: 'break-word', whiteSpace: 'pre-wrap' }} data-testid="collapsable-text-content">
       {renderedText}
       {text.length > truncBy && (
-        <MoreLessButton onClick={onClickMoreLess} data-testid="collapsable-text-moreless-btn">
+        <MoreLessButton
+          onClick={(e) => {
+            onClickMoreLess?.(e);
+            handleClickMoreLess();
+          }}
+          data-testid="collapsable-text-moreless-btn"
+        >
           {expanded ? ' 접기' : ' 더보기'}
         </MoreLessButton>
       )}
