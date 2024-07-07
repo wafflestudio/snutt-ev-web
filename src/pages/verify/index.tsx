4f65dca5a39443c773d4320490afd0d49859465f
ars-ki-00 <dkwanm1@snu.ac.kr>
2024-07-07 13:47:16 +0900

    임시 페이지 노출 제거

--- src/pages/verify/index.tsx
+++ src/pages/verify/index.tsx
@@ -1,29 +1,28 @@
-// import { MailVerifyImpl } from '@/pageImpl/mailVerifyImpl';
-import styled from '@emotion/styled';
-
-import SvgTimetableOn from '@/components/atoms/Icons/SvgTimetableOn';
-import { SvgWaffleCat } from '@/components/atoms/Icons/SvgWaffleCat';
-import { Title01, Title02 } from '@/components/atoms/Typography';
-import { AppBar } from '@/components/molecules/AppBar';
+import { MailVerifyImpl } from '@/pageImpl/mailVerifyImpl';
+// import styled from '@emotion/styled';
+// import SvgTimetableOn from '@/components/atoms/Icons/SvgTimetableOn';
+// import { SvgWaffleCat } from '@/components/atoms/Icons/SvgWaffleCat';
+// import { Title01, Title02 } from '@/components/atoms/Typography';
+// import { AppBar } from '@/components/molecules/AppBar';
 import { withGetServerSideProps } from '@/utils/withGetServersideProps';
 
 export default function Verify() {
-  return (
-    <>
-      <AppBar left={<SvgTimetableOn height={30} width={30} />}>
-        <Title01 style={{ marginLeft: 12 }}>강의평</Title01>
-      </AppBar>
-      <Container>
-        <Title02 style={{ marginBottom: 40, textAlign: 'center' }}>
-          <br />
-          페이지를 찾을 수 없습니다
-        </Title02>
-        <SvgWaffleCat />
-        <OurName>@wafflestudio</OurName>
-      </Container>
-    </>
-  );
-  // return <MailVerifyImpl />;
+  // return (
+  //   <>
+  //     <AppBar left={<SvgTimetableOn height={30} width={30} />}>
+  //       <Title01 style={{ marginLeft: 12 }}>강의평</Title01>
+  //     </AppBar>
+  //     <Container>
+  //       <Title02 style={{ marginBottom: 40, textAlign: 'center' }}>
+  //         <br />
+  //         페이지를 찾을 수 없습니다
+  //       </Title02>
+  //       <SvgWaffleCat />
+  //       <OurName>@wafflestudio</OurName>
+  //     </Container>
+  //   </>
+  // );
+  return <MailVerifyImpl />;
 }
 
 export const getServerSideProps = withGetServerSideProps(
@@ -33,17 +32,17 @@ export const getServerSideProps = withGetServerSideProps(
   { emailVerification: 'not-verified' },
 );
 
-const Container = styled.div`
-  width: 100%;
-  height: 85vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
+// const Container = styled.div`
+//   width: 100%;
+//   height: 85vh;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+// `;
 
-const OurName = styled.div`
-  font-family: AppleSDGothicNeo;
-  font-size: 9px;
-  margin-top: 25px;
-`;
+// const OurName = styled.div`
+//   font-family: AppleSDGothicNeo;
+//   font-size: 9px;
+//   margin-top: 25px;
+// `;
