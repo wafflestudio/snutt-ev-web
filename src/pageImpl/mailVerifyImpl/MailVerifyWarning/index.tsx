8ea7a1ee330c1fc534d3640833b653323b9216e5
KiWan KIM <dkwanm00@gmail.com>
2026-01-14 20:47:33 +0900

    이메일 인증 에러 메시지 방식 수정 (#368)

--- src/pageImpl/mailVerifyImpl/MailVerifyWarning/index.tsx
+++ src/pageImpl/mailVerifyImpl/MailVerifyWarning/index.tsx
@@ -1,26 +1,15 @@
 import styled from '@emotion/styled';
 
 import { Detail } from '@/components/atoms/Typography';
-import { MailVerificationState } from '@/pageImpl/mailVerifyImpl';
 import { COLORS } from '@/styles/colors';
 
-type Props = { state: MailVerificationState };
+type Props = { message: string | null };
 
-export const MailVerifyWarning = ({ state }: Props) => {
-  const WARINING = {
-    [MailVerificationState.TIMEOUT]: '인증요청에 실패했습니다. 다시 시도해주세요',
-    [MailVerificationState.INVALID_NUMBER]: '인증번호가 틀렸습니다. 다시 시도해주세요',
-    [MailVerificationState.ALREADY_VERIFIED]: '이미 인증된 계정입니다',
-    [MailVerificationState.VERFIED_FROM_OTHER_MAIL]: '이미 사용된 메일입니다',
-    [MailVerificationState.TOO_MANY_REQUEST]: '인증요청에 실패했습니다. 3분 후에 다시 시도해주세요',
-    [MailVerificationState.NONE]: '',
-    [MailVerificationState.READY]: '',
-  };
-
+export const MailVerifyWarning = ({ message }: Props) => {
   return (
     <WarningText>
       <Detail style={{ color: COLORS.red }} data-testid="verify-warning-text">
-        {WARINING[state]}
+        {message}
       </Detail>
     </WarningText>
   );
